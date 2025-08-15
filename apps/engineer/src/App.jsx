28a9ab479a64c00b2050aa351a887185f0ba98a4
John Pfeiffer <foupfeiffer@gmail.com>
2025-08-14 21:38:40 -0700

    copy paste for multi SPA

---
+++ apps/engineer/src/App.jsx
@@ -0,0 +1,28 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Software Engineer Links <span className="heart">♥️</span> React
+        </p>
+        <p className="small">
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ apps/manager/src/App.jsx
@@ -0,0 +1,28 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          People Manager Links <span className="heart">♥️</span> React
+        </p>
+        <p className="small">
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
